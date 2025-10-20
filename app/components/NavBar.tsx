25a9527160ac521c0b9b90b95b4f0e291068aef5
kolaomoyajowo-blip <kolaomoyajowo@gmail.com>
2025-10-20 11:35:26 -0500

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,18 +1,28 @@
 "use client";
 
 import Link from "next/link";
+import Logo from "./Logo";
 
 export default function NavBar() {
   return (
     <header className="border-b">
       <nav className="mx-auto flex max-w-5xl items-center justify-between px-4 py-3">
-        <Link href="/" className="text-lg font-semibold">
-          Privacy Intelligence Academy
+        {/* Logo on the left, links on the right */}
+        <Link href="/" aria-label="Go to homepage">
+          <Logo />
         </Link>
 
         <ul className="flex items-center gap-6 text-sm">
-          <li><Link href="/about" className="hover:underline">About</Link></li>
-          <li><Link href="/blog" className="hover:underline">Blog</Link></li>
+          <li>
+            <Link href="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog" className="hover:underline">
+              Blog
+            </Link>
+          </li>
           <li>
             <Link
               href="/contact"
